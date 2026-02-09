ae5df7deb1b99acd9435faaaa65fc24df8bac514
Aravind-Singaraja <aravinds448@gmail.com>
2026-02-09 16:22:44 +0530

    mobile view fix

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 AOS.init({
-    duration: 900,
-    once: true,
-    easing: 'ease-out-cubic'
+  once: true,
+  duration: 700,
+  disable: window.innerWidth < 768
 });
 bootstrap.Carousel.getOrCreateInstance(
-    document.querySelector('#heroSlider')
+  document.querySelector('#heroSlider')
 )
 const sections = document.querySelectorAll("section[id]");
-  const navLinks = document.querySelectorAll(".navbar .nav-link");
+const navLinks = document.querySelectorAll(".navbar .nav-link");
 
-  window.addEventListener("scroll", () => {
-    let current = "";
+window.addEventListener("scroll", () => {
+  let current = "";
 
-    sections.forEach(section => {
-      const sectionTop = section.offsetTop - 120;
-      if (pageYOffset >= sectionTop) {
-        current = section.getAttribute("id");
-      }
-    });
-
-    navLinks.forEach(link => {
-      link.classList.remove("active");
-      if (link.getAttribute("href") === "#" + current) {
-        link.classList.add("active");
-      }
-    });
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 120;
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute("id");
+    }
   });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
